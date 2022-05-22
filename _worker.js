b6cc36fb29940a863ce446f1dd256816cb4317e0
smills0e <93284696+smills0e@users.noreply.github.com>
2022-05-23 03:59:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'shiny-band-2ef0.bblair9981.workers.dev'
+      url.hostname = 'bocik-gbox9981.koyeb.app'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
