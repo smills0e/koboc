dd5e19268a1a1b4aa775bb79d8b6c8a8d2a0fdf4
smills0e <93284696+smills0e@users.noreply.github.com>
2022-05-23 03:55:38 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'gzcf.us-south.cf.appdomain.cloud'
+      url.hostname = 'shiny-band-2ef0.bblair9981.workers.dev'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
